93c437a035c9766fad434bea49c6ee018ead75bc
Dasha <dashakonopelko@gmail.com>
2025-04-28 20:07:24 +0300

    helper 2

--- 04_MultiClient_Server_Tcp/Client/MainWindow.xaml.cs
+++ 04_MultiClient_Server_Tcp/Client/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,9 +28,40 @@ namespace Client
             InitializeComponent();
         }
 
-        private void GetFactorial_Btn(object sender, RoutedEventArgs e)
+        private async void GetFactorial_Btn(object sender, RoutedEventArgs e)
         {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(ipBox.Text), int.Parse(portBox.Text));
+            string message = txtBox.Text;
 
+            TcpClient client = null;
+
+            try
+            {
+                client = new TcpClient();
+                await client.ConnectAsync(ipPoint);
+                NetworkStream stream = client.GetStream();
+
+                StreamWriter sw = new StreamWriter(stream);
+                await sw.WriteLineAsync(message);
+                await sw.FlushAsync();
+
+                StreamReader sr = new StreamReader(stream);
+                string result = await sr.ReadLineAsync();
+                sr.Close();
+                sw.Close();
+                stream.Close();
+
+                list.Items.Add(result);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                client?.Close();
+            }
         }
     }
 }
